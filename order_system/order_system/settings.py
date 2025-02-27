57543a2518532b308a80fe3288fe00c9b3ff6de1
alex <alex_77_90@mail.ru>
2025-02-27 20:04:53 +0500

    update settings.py

--- order_system/order_system/settings.py
+++ order_system/order_system/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ky-$7g9_$ylp4pd194)6to@b-$*!n^ct9qfl$_k&wf@2+jy(xf'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-ky-$7g9_$ylp4pd194)6to@b-$*!n^ct9qfl$_k&wf@2+jy(xf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': 'order_db',
         'USER': 'order_user',
         'PASSWORD': '1234',
-        'HOST': 'db',
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
         'PORT': 5432
     }
 }
